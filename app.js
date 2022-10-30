4d58c7ccc7eedf3cfed299e74bfb80514ff08246
Ilya Kruglyak <bcrash1@gmail.com>
2022-10-30 18:57:27 +0200

    HW11 in Class

--- app.js
+++ app.js
@@ -12,21 +12,36 @@
 
 
 
-function sumDigits(number) {
-    let a = 0;
-    let res = 0;
+// function sumDigits(number) {             hw 11
+//     let a = 0;
+//     let res = 0;
 
-    while (number != 0) {
-        if (number < 0) {
-            number *= -1;
-        }
+//     while (number != 0) {
+//         if (number < 0) {
+//             number *= -1;
+//         }
 
-        a = number % 10;
-        res += a;
-        number = Math.floor(number / 10);
-    }
+//         a = number % 10;
+//         res += a;
+//         number = Math.floor(number / 10);
+//     }
 
-    return res;
+//     return res;
+// }
+
+// console.log(sumDigits(0123));
+
+
+
+function sumDigits(number) {            // hw 11 in class
+    number = Math.abs(number);
+    number = Math.trunc(number);
+    let sum = 0;
+    do {
+        let digit = number % 10;
+        number = (number - digit) / 10;
+        sum += digit;
+    }while(number != 0);
+    return sum;
 }
-
-console.log(sumDigits(1010)); //should be printed out 6
+console.log(sumDigits(-623));
