d410537ac653bc0ebd9748dddb7191c418c9c4f2
Ilya Kruglyak <bcrash1@gmail.com>
2022-12-05 21:28:24 +0200

    Class Solution of HW21

--- src/form.js
+++ src/form.js
@@ -1,36 +1,77 @@
 const inputElements = document.querySelectorAll(".form-class [name]");
-const errorMsg = document.querySelector(".error");
-const minSalary = 1000;
-const maxSalary = 40000;
-const timerSet = 5000;
-datePickerId.max = new Date().toISOString().split("T")[0];    //gets current date 
-    
+const MIN_SALARY = 1000;
+const MAX_SALARY = 40000;
+const MIN_YEAR = 1950;
+const maxYear = getMaxYear();
+const TIME_OUT_ERROR_MESSAGE = 5000;
+const ERROR_CLASS = "error";
+const company = new Company();
 
-function onSubmit(event){
+const dateErrorElement = document.getElementById("date_error");
+const salaryErrorElement = document.getElementById("salary_error");
+
+function onSubmit(event) {
     event.preventDefault();
     console.log("submitted");
-    const employee = Array.from(inputElements).reduce((res, curr) => {
-        res[curr.name] = curr.value;
-        return res;
-    }, {}
+    const employee = Array.from(inputElements).reduce(
+        (res, cur) => {
+            res[cur.name] = cur.value;
+            return res;
+        }, {}
     )
-    console.log(employee);
+    console.log(employee)
+    company.hireEmployee(employee);
+    
 }
-
 function onChange(event) {
-    if(event.target.name == "salary") {
-        if(+event.target.value < minSalary || +event.target.value > maxSalary) {
-            event.target.value=''
-            error(event, `salary need to be between ${minSalary} and ${maxSalary}`)
-            
-        }
+
+    if (event.target.name == "salary") {
+        validateSalary(event.target)
+    } else if (event.target.name == "birthDate") {
+        validateBirthdate(event.target);
     }
 }
-function error(event, message) {
-    event.target.value=''
-    errorMsg.innerHTML = "Error: " + message;
-    timer = setTimeout(() => {
-        errorMsg.innerHTML = '';
-    }, timerSet)
+function validateSalary(element) {
+    const value = +element.value;
+    if (value < MIN_SALARY || value > MAX_SALARY) {
+        const message = value < MIN_SALARY ? `salary must be ${MIN_SALARY} or greater`
+            : `salary must be ${MAX_SALARY} or less`;
+        showErrorMessage(element, message, salaryErrorElement);
+    }
 
+}
+function validateBirthdate(element) {
+    const value = +element.value.slice(0, 4);
+    if (value < MIN_YEAR || value > maxYear) {
+        const message = value < MIN_YEAR ? `year must be ${MIN_YEAR} or greater`:
+             `year must be ${maxYear} or less`;
+        showErrorMessage(element, message, dateErrorElement) ;    
+
+    }
+
+}
+function showErrorMessage(element, message, errorElement) {
+    element.classList.add(ERROR_CLASS);
+    errorElement.innerHTML = message;
+    setTimeout(() => {
+        element.classList.remove(ERROR_CLASS);
+        element.value = ''; 
+        errorElement.innerHTML = '';
+    }, TIME_OUT_ERROR_MESSAGE);
+}
+
+function getMaxYear() {
+    return new Date().getFullYear();
+}
+function Company() {
+    this.employees = [];
+}
+Company.prototype.hireEmployee = function(employee) {
+    this.employees.push(employee);
+}
+Company.prototype.getAllEmployees = function(){
+    return this.employees;
+}
+Company.prototype.getEmployeesBySalary = function(salaryFrom, salaryTo) {
+    //TODO
 }
